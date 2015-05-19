b5664a56a51157a734f89c3e65d03b9717f0fec9
mbernier <matt.bernier@sendgrid.com>
2015-05-19 09:53:52 -0700

    Changed how we parse page content, added meta
    moved the order around and made getting the data from the page dependant on whether we have a redirect page or not

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
-var html_parser = require('./html.js');
+var html_parser = require('./html.js'),
+    store = require('./store.js');
 
 var express = require('express'),
 	fs = require('fs'),
@@ -9,13 +10,13 @@ var express = require('express'),
 var app = express();
 
 //What is the base URL for the site? Used to determine on/off site links
-process.env['base_url'] = "BASE URL HERE";
+process.env['base_url'] = "https://sendgrid.com/docs/";
 
 //what is the GITHUB URL for your site? Used to find github links for this repo/file
-process.env['github_repo'] = "GITHUB URL HERE";
+process.env['github_repo'] = "https://github.com/sendgrid/docs";
 
 //the URL of the page we're scraping
-process.env['page_url'] = 'YOUR URL HERE';
+process.env['page_url'] = 'https://sendgrid.com/docs/Apps/index.html';
 
 var output = '';
 
@@ -23,22 +24,21 @@ var serve = true;
 
 var $ = cheerio;
 
-
 app.get('/page/:id', function(req, res){
     /*
      This should look up the info about the page:id and display it
-    */
+     */
 });
 
 app.get('/images', function(req, res){
-   /*
+    /*
      Show all the images in the docs
      */
 });
 
 app.get('/tasks', function(req, res) {
     /*
-    List all the tasks in order of score
+     List all the tasks in order of score
      */
 });
 
@@ -51,7 +51,7 @@ app.get('/task/:id', function(req, res) {
 app.post('/task', function(req, res) {
     /*
      accept a new task, calculate the score
-      */
+     */
 });
 
 
@@ -67,46 +67,46 @@ app.get('/scrape', function(req, res) {
             $ = $.load(html);
 
             /*
-                @todo is this a redirect page?
-                    Check for the signs of this being a redirect page - meta redirect and canonical
-                    Store on the page where this is redirecting to that there is a redirect pointing there.
+             @todo is this an abandoned page?
+             Look at the flag for "status": live, dark-deploy, abandoned, tentative-abandoned
+             If this page has "dark-deploy" get the menu from the page and parse it
+             If this page has "abandoned" get the menu from the page and parse it
+             If this page has no status flag or is "live":
+             If menu list hash is not set:
+             then get the links in the menu, sort and hash
+             This should reference a menu object hash and compare to the menu going forward.
+             If the menu hash is different
+             it should re-parse the menu list for the differences (aka dark deploy or abandoned pages) and
+             set the tentative-abandoned flag
+             Otherwise,
+             set the status page as "live"
 
-                @todo is this an abandoned page?
-                    Look at the flag for "status": live, dark-deploy, abandoned, tentative-abandoned
-                    If this page has "dark-deploy" get the menu from the page and parse it
-                    If this page has "abandoned" get the menu from the page and parse it
-                    If this page has no status flag or is "live":
-                        If menu list hash is not set:
-                            then get the links in the menu, sort and hash
-                        This should reference a menu object hash and compare to the menu going forward.
-                        If the menu hash is different
-                            it should re-parse the menu list for the differences (aka dark deploy or abandoned pages) and
-                              set the tentative-abandoned flag
-                        Otherwise,
-                            set the status page as "live"
-
-                @todo abandoned page report:
-                        The docs tracker user should be able to see a list of new pages where they can mark them as "dark deployed"
-                        or abandoned instead of tentative-abandoned.
+             @todo abandoned page report:
+             The docs tracker user should be able to see a list of new pages where they can mark them as "dark deployed"
+             or abandoned instead of tentative-abandoned.
              */
-            var link_objs = $('a');
-            var img_objs = $('img');
 
-            output += html_parser.images(img_objs);
+            //get the meta values from the page
+            var meta = $('meta');
 
             /*
+            If this returns false, we're done because this is a redirect page
+                It will also handle grabbing the other meta information from the page if this is not a redirect
+             */
+            if (meta.length==0 || html_parser.meta(meta)) {
 
-            Get the link information
+                //Get the images from the page
+                var img_objs = $('img');
+                output += html_parser.images(img_objs);
 
-            */
-
-            output += html_parser.links(link_objs);
-
+                //Get the link information
+                var link_objs = $('a');
+                output += html_parser.links(link_objs);
+            }
         }
 
         res.send(output);
     });
-
 });
 
 app.post('/urls', function(req, res) {
@@ -118,7 +118,6 @@ app.post('/urls', function(req, res) {
      */
 });
 
-
-    app.listen('4000')
-    console.log('Server running on http://localhost:4000');
-    exports = module.exports = app;
+app.listen('4000')
+console.log('Server running on http://localhost:4000');
+exports = module.exports = app;
